boundarydevices/bootloaders_build

commit d8a121a365352b38c960b21eab44ad07b33f336e

release_android: remove all previous build files
Before copying the newly built binaries, make sure that the old ones are
not remove first. This will avoid keeping in the git history old binaries
that are not built and updated anymore.

Signed-off-by: Esteban Blanc <eblanc@baylibre.com>

release_android.sh
@@ -147,6 +147,9 @@ function main {
                 fi
             fi
 
+            # Clean output directory
+            rm -rvf "${aosp}/${mtk_binaries_path}/"*
+
             # Copy binaries
             out_dir=$(out_dir "${mtk_config}" "${mode}")
             cp -r "${out_dir}/"* "${aosp}/${mtk_binaries_path}"

commit bf7b9155ceaca4a0b7bce7f9c0a66ae40e67d641

release_android: clean only optee-ta output directory
When we run release_android script, we build binaries for release and
userdebug mode.
We should not remove the whole output directory, otherwise the output
directory will only contains the binaries from the last build.
We need to have both binaries: release and userdebug.

Except for the optee-ta binaries, we only use the release binaries.
This folder can be cleaned before copying all the binaries.

Fixes: d8a121a release_android: remove all previous build files
Signed-off-by: Julien Masson <jmasson@baylibre.com>

release_android.sh
@@ -147,8 +147,8 @@ function main {
                 fi
             fi
 
-            # Clean output directory
-            rm -rvf "${aosp}/${mtk_binaries_path}/"*
+            # Clean optee-ta output directory
+            rm -rvf "${aosp}/${mtk_binaries_path}/optee-ta/"*
 
             # Copy binaries
             out_dir=$(out_dir "${mtk_config}" "${mode}")